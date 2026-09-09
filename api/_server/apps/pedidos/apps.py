cca2aa5a8bacc698daf66d0fdf10502a79deffd1
TonyVJ78 <gasorpez91b@gmail.com>
2026-09-09 17:40:22 -0400

    fix(build): remove pyproject.toml to install from requirements.txt and clean legacy api

--- api/_server/apps/pedidos/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PedidosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.pedidos'
-    verbose_name = 'Comercio Electrónico y Ventas'
--- api/_server/apps/tiendas/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TiendasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.tiendas'
-    verbose_name = 'Tiendas'
